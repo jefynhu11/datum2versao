c5547ed8588988850340daff566e181274c78929
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2021-02-24 12:49:04 -0300

    Adicionei anotação para especifico CSV

--- src/test/java/org/example/testcases/BuyTwoProductsTestCase.java
+++ src/test/java/org/example/testcases/BuyTwoProductsTestCase.java
@@ -5,6 +5,8 @@ import org.example.framework.webdrivers.DriverManager;
 import org.example.tasks.AddProductsTask;
 import org.example.tasks.AuthenticationTask;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.openqa.selenium.WebDriver;
 
 public class BuyTwoProductsTestCase extends BaseTest {
@@ -13,10 +15,11 @@ public class BuyTwoProductsTestCase extends BaseTest {
     private final AddProductsTask addProductsTask = new AddProductsTask(driver);
     private final AuthenticationTask authenticationTask = new AuthenticationTask(driver);
 
-    @Test
-    public void test() throws InterruptedException {
+    @ParameterizedTest
+    @CsvFileSource(resources = "/dado.csv")
+    public void test(String email, String password) throws InterruptedException {
         addProductsTask.adicionaCarrinhoProdutos();
-        authenticationTask.authenticationAccount("email", "password");
+        authenticationTask.authenticationAccount(email, password);
 
         Thread.sleep(5000);
     }
