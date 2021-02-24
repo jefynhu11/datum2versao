ac1497a51a6ddd88867c073e02af152a774f516c
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2021-02-24 16:21:06 -0300

    Import classe purchaseTask

--- src/test/java/org/example/testcases/BuyTwoProductsTestCase.java
+++ src/test/java/org/example/testcases/BuyTwoProductsTestCase.java
@@ -4,7 +4,7 @@ import org.example.framework.supports.BaseTest;
 import org.example.framework.webdrivers.DriverManager;
 import org.example.tasks.AddProductsTask;
 import org.example.tasks.AuthenticationTask;
-import org.junit.jupiter.api.Test;
+import org.example.tasks.PurchaseProductsTask;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.openqa.selenium.WebDriver;
@@ -14,12 +14,14 @@ public class BuyTwoProductsTestCase extends BaseTest {
     private final WebDriver driver = DriverManager.getDriver();
     private final AddProductsTask addProductsTask = new AddProductsTask(driver);
     private final AuthenticationTask authenticationTask = new AuthenticationTask(driver);
+    private final PurchaseProductsTask purchaseProductsTask = new PurchaseProductsTask(driver);
 
     @ParameterizedTest
     @CsvFileSource(resources = "/dado.csv")
     public void test(String email, String password) throws InterruptedException {
         addProductsTask.adicionaCarrinhoProdutos();
         authenticationTask.authenticationAccount(email, password);
+        purchaseProductsTask.purchaseProducts();
 
         Thread.sleep(5000);
     }
