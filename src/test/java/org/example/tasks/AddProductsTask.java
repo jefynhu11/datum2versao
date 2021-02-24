64e97cd040e47c251a6cfac7a2edd2ecfcf48630
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2021-02-24 19:42:49 -0300

    Import 'wait' e adicionei borda para elemento

--- src/test/java/org/example/tasks/AddProductsTask.java
+++ src/test/java/org/example/tasks/AddProductsTask.java
@@ -1,5 +1,7 @@
 package org.example.tasks;
 
+import org.example.framework.supports.Wait;
+import org.example.framework.tools.JsExecutor;
 import org.example.pageobjetcs.AddProductsPageObjects;
 import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.WebDriver;
@@ -8,15 +10,19 @@ public class AddProductsTask {
 
     private final WebDriver driver;
     private final AddProductsPageObjects addProductsPageObjects;
+    private final Wait wait;
 
     public AddProductsTask(WebDriver driver) {
         this.driver = driver;
+        wait = new Wait(driver);
         addProductsPageObjects = new AddProductsPageObjects(driver);
     }
 
     public void adicionaCarrinhoProdutos() {
+        JsExecutor.highLightElement(driver,addProductsPageObjects.getBlouseProductButton());
         addProductsPageObjects.getBlouseProductButton().click();
         addProductsPageObjects.getIframe();
+        JsExecutor.highLightElement(driver, addProductsPageObjects.getAddToCartButton());
         addProductsPageObjects.getAddToCartButton().click();
         addProductsPageObjects.getIframeContinueShoppingButton().click();
         addProductsPageObjects.getIframeDefalut();
